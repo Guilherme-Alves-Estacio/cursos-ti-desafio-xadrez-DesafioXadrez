9cb29208abd40d4a5670cd23e53759415fee37d6
Guilherme-Alves-Estacio <202602126362@alunos.estacio.br>
2026-05-21 01:06:13 +0000

    Update Desafio Mestre Xadrez

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,46 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+// Nível Mestre
+void recursaoTorre(int casas)
+{
+    if (casas > 0)
+    {
+        printf("Movimento da Torre: Direita\n");
+        recursaoTorre(casas - 1);
+    }
+}
+
+void recursaoBispo(int casas)
+{
+    if (casas >= 5)
+    {
+        return;
+    }
+
+    for (int i = 0; i < 1; i++)
+    {
+        printf("Movimento do Bispo: Direita\n");
+    }
+
+    printf("Movimento do Bispo: Cima\n");
+    printf("\n");
+
+    recursaoBispo(casas + 1);
+}
+
+void recursaoRainha(int casas)
+{
+    if (casas <= 0)
+    {
+        return;
+    }
+    printf("Movimento da Rainha: Esquerda\n");
+    recursaoRainha(casas - 1);
+}
+
+// Nível Mestre
+
 int main()
 {
     // Nível Novato - Movimentação das Peças
@@ -53,6 +93,30 @@ int main()
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+
+    recursaoTorre(5);
+    printf("\n");
+    recursaoBispo(0);
+    recursaoRainha(8);
+    printf("\n");
+
+    // Cavalo
+
+    for (int c = 0; c < 3; c++)
+    {
+
+        if (c < 2)
+        {
+            printf("Movimento do Cavalo: Cima\n");
+            continue;
+        }
+
+        for (int d = 0; d < 2; d++)
+        {
+            printf("Movimento do Cavalo: Direita\n");
+            break;
+        }
+    }
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
